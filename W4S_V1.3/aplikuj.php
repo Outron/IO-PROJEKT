e3b22ca6ed34fba27005555d2bec7718d3fb6846
dominiol <91890313+dominiol@users.noreply.github.com>
2023-06-28 02:41:57 +0200

    poprawka do aplikuj.php

--- W4S_V1.3/aplikuj.php
+++ W4S_V1.3/aplikuj.php
@@ -19,16 +19,22 @@ if (isset($_SESSION['user']) && $_SESSION['typ_uzytkownika'] == "student" && iss
         echo "Już aplikowałeś na tę ofertę!";
         header("refresh:3;url=oferty.php");
     } else {
+        $sql_oferty = "SELECT * FROM oferty WHERE id = $id_oferty";
+        $result_oferty = mysqli_query($conn, $sql_oferty);
+        $oferta = mysqli_fetch_assoc($result_oferty);
+        $tytul_oferty = $oferta['tytul_oferty'];
+
         $sql = "SELECT * FROM pracownicy WHERE id = $id_uzytkownika";
         $result = mysqli_query($conn, $sql);
         $user = mysqli_fetch_assoc($result);
 
-        echo "<h2>Aplikuj na ofertę</h2>";
+        echo "<h2>Aplikuj na ofertę o ID ".$id_oferty." - ".$tytul_oferty."</h2>";
         echo "<form method='POST' action='zapisz_aplikacje.php'>";
         echo "<input type='hidden' name='id_oferty' value='$id_oferty'>";
         echo "<input type='hidden' name='id_uzytkownika' value='$id_uzytkownika'>";
         echo "Kwalifikacje: <input type='text' name='kwalifikacje'><br>";
         echo "Chęci: <input type='text' name='checi'><br>";
+        echo "Pracodawca będzie mógł zobaczyć twoje imię, nazwisko, numer telefonu oraz adres email.<br><br>";
         echo "<input type='submit' value='Aplikuj' name='submit'>";
         echo "</form>";
     }

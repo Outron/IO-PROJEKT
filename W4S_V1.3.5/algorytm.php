0bfbfc0b0591c8f7aa30854925da37b2f6762673
konrkra117 <konrkra117@student.polsl.pl>
2023-06-30 17:53:42 +0200

    ALGORYTM DONE

--- W4S_V1.3.5/algorytm.php
+++ W4S_V1.3.5/algorytm.php
@@ -1,9 +1,5 @@
-<pre><?php
-	// Narazie suorwy skrypt do ogarniecia tego algorytmu jak to ma dzialac
-
-	// Docelowo wykonywanie w oferty.php
-	// Pokazywanie w liscie ofert przy danej ofercie jako duzym drukiem procent
-
+<?php
+// Funkcja dopasowania godzin studenta do oferty
 function algo_dopasowania_godzin($student, $oferta) {
 	// Rozbij na listę, żeby był dostęp do elementów
 	$_student = [];
@@ -39,7 +35,31 @@ function algo_dopasowania_godzin($student, $oferta) {
 			($_student[$i] == "wolne") && ($_oferta[$i] != "wolne") // student wolny
 		) {
 			$wynik += 1/7;
-		} else {}
+		} else {
+			$stud_od_do = explode("-", str_replace(":", "", $_student[$i]));
+			$stud_od = intval($stud_od_do[0]);
+			$stud_do = intval($stud_od_do[1]);
+			$stud = $stud_do - $stud_od;
+
+			$off_od_do = explode("-", str_replace(":", "", $_oferta[$i]));
+			$off_od = intval($off_od_do[0]);
+			$off_do = intval($off_od_do[1]);
+			$off = $off_do - $off_od;
+
+			$dopasowanie = $stud - $off;
+
+			if ($dopasowanie < 0) {
+				// Oferta wymaga wiecej godzin niz ma student
+				// Jakis wspolczynnik obrazujacy niedopasowanie
+				$wynik += (($stud/$off)/7);
+			} else {
+				// Student ma wiecej dostepnosci niz oferta
+				// czyli OK
+				$wynik += 1/7;
+			}
+
+			echo "$stud - $off => $dopasowanie\n";
+		}
 
                 echo "wynik czastkowy: $wynik";
 
@@ -52,16 +72,14 @@ function algo_dopasowania_godzin($student, $oferta) {
 
 
 // Pobierz dostepnosc z bazy dla studenta i z oferty pracy konkretnej
-$g_student = explode(";", "Cały tydzień,08:00-16:00");
+//$g_student = explode(";", "Cały tydzień,08:00-16:01");
 //$g_student = explode(";", "pn,08:00-16:00;wt,08:00-16:00;sr,08:00-16:00;cz,08:00-16:00;pt,08:00-16:00;sb,wolne;nd,wolne");
-$g_oferta = explode(";", "Cały tydzień,08:00-16:00");
-//$g_oferta = explode(";", "Cały tydzień,08:00-16:00;Cały tydzień,08:00-16:00;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01");
-$algowynik = algo_dopasowania_godzin($g_student, $g_oferta);
+//$g_oferta = explode(";", "Cały tydzień,wolne");
+//$g_oferta = explode(";", "Cały tydzień,00:00-16:00;Cały tydzień,03:00-16:00;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01;Cały tydzień,08:00-15:01;Cały tydzień,wolne;Cały tydzień,wolne");
+//$algowynik = algo_dopasowania_godzin($g_student, $g_oferta);
 
-echo "Dopasowanie: ". $algowynik ." %";
-
-exit();
-
+//echo "Dopasowanie: ". $algowynik ." %";
+?>
 
 
 
@@ -71,13 +89,8 @@ exit();
 
 
 
-
-
-
-
-
-
-
+<?php
+/*
 	var_dump($_student);
 	echo "<br>";
 	var_dump($_oferta);
@@ -125,6 +138,5 @@ exit();
 	}
 
 	echo "<br><br>Wynik porownania: " . round($wynik*100, 2) . " %";
+*/
 ?>
-
-</pre>
